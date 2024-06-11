95ed368c70af1d61e04524e6b6c23346355262b2
Kauan <kauan.s.leite@hotmail.com>
2024-06-10 23:11:53 -0300

    start project with tailwind

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-red-700">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        teste
+      </button>
+      <h1>Hero</h1>
+    </div>
+  );
+}
